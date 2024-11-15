9a5a7ea793c17cb4e2f2f73682a66e4b4baa872e
evellynliena <evellynliena66@gmail.com>
2024-11-15 15:39:18 +0700

    Add files via upload

---
+++ matematika.py
@@ -0,0 +1,7 @@
+pi == 3.14159
+def luas_lingkaran(jari2):
+    return pi * (jari2**2)
+def luas_persegi(sisi):
+    return sisi * sisi  
+def luas_persegi_panjang(panjang, lebar):
+    return panjang * lebar
